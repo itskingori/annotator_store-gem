86cc7077ec20158538839cc8600df9d857a7510d
King'ori Maina <j@kingori.co>
2014-10-09 22:32:55 +0200

    Check api description endpoint json

--- spec/dummy/spec/requests/pages_spec.rb
+++ spec/dummy/spec/requests/pages_spec.rb
@@ -7,5 +7,10 @@ RSpec.describe 'Pages', type: :request do
       get annotator_store.root_path
       expect(response.status).to be(200)
     end
+
+    it 'returns the json api description' do
+      get annotator_store.root_path
+      expect(response).to match_json_schema('api/description')
+    end
   end
 end
