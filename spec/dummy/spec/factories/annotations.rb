7d5bd4d5825792f26effa17e3e99dd4af5863186
King'ori Maina <j@kingori.co>
2014-10-13 18:12:41 +0200

    Rename AnnotatorStore::Annotation factory

--- spec/dummy/spec/factories/annotations.rb
+++ spec/dummy/spec/factories/annotations.rb
@@ -1,8 +1,7 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :annotator_store_annotation, class: AnnotatorStore::Annotation do
-    id '814865cd-5a1d-4771-9306-4268f188fe9e'
+  factory :annotation, class: AnnotatorStore::Annotation do
     version 'v1.0'
     text 'A note I wrote'
     quote 'the text that was annotated'
--- spec/dummy/spec/models/annotator_store/annotation_spec.rb
+++ spec/dummy/spec/models/annotator_store/annotation_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 module AnnotatorStore
   RSpec.describe Annotation, type: :model do
 
-    let(:annotation) { FactoryGirl.create :annotator_store_annotation }
+    let(:annotation) { FactoryGirl.create :annotation }
 
     it 'has a valid factory' do
       expect(annotation).to be_valid
