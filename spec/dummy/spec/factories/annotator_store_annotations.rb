e26d88005c9afa13363d6d6a60e4c0444d5307d6
King'ori Maina <j@kingori.co>
2014-10-13 16:29:11 +0200

    Fix code styling errors

--- spec/dummy/spec/factories/annotator_store_annotations.rb
+++ spec/dummy/spec/factories/annotator_store_annotations.rb
@@ -2,11 +2,11 @@
 
 FactoryGirl.define do
   factory :annotator_store_annotation, class: AnnotatorStore::Annotation do
-    id "814865cd-5a1d-4771-9306-4268f188fe9e"
-    version "v1.0"
-    text "A note I wrote"
-    quote "the text that was annotated"
-    uri "http://example.com"
-    ranges "{}"
+    id '814865cd-5a1d-4771-9306-4268f188fe9e'
+    version 'v1.0'
+    text 'A note I wrote'
+    quote 'the text that was annotated'
+    uri 'http://example.com'
+    ranges '{}'
   end
 end
--- spec/dummy/spec/models/annotator_store/annotation_spec.rb
+++ spec/dummy/spec/models/annotator_store/annotation_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 module AnnotatorStore
   RSpec.describe Annotation, type: :model do
 
-     let(:annotation) { FactoryGirl.create :annotator_store_annotation }
+    let(:annotation) { FactoryGirl.create :annotator_store_annotation }
 
-     it 'has a valid factory' do
+    it 'has a valid factory' do
       expect(annotation).to be_valid
     end
   end
