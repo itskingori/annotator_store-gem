1c35d918eea21acc20bfcf26ad6f5780957bb215
King'ori Maina <j@kingori.co>
2014-10-15 11:38:43 +0200

    Fix CORS issue

--- app/controllers/annotator_store/application_controller.rb
+++ app/controllers/annotator_store/application_controller.rb
@@ -1,4 +1,15 @@
 module AnnotatorStore
   class ApplicationController < ActionController::Base
+    before_filter :set_headers
+
+    private
+
+    def set_headers
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Expose-Headers'] = 'ETag'
+      headers['Access-Control-Allow-Methods'] = 'GET, POST, PATCH, PUT, DELETE, OPTIONS, HEAD'
+      headers['Access-Control-Allow-Headers'] = '*,x-requested-with,Content-Type,If-Modified-Since,If-None-Match'
+      headers['Access-Control-Max-Age'] = '86400'
+    end
   end
 end
