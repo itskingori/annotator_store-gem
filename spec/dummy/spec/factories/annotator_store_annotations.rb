e1c62ffc7fd79276100db3fba50f6c40e6eedbbe
King'ori Maina <j@kingori.co>
2014-10-20 08:10:32 +0200

    Namespace AnnotatorStore::Annotations factory

--- spec/dummy/spec/factories/annotator_store_annotations.rb
+++ spec/dummy/spec/factories/annotator_store_annotations.rb
@@ -1,11 +1,10 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :annotation, class: AnnotatorStore::Annotation do
+  factory :annotator_store_annotation, class: AnnotatorStore::Annotation do
     version 'v1.0'
     text 'A note I wrote'
     quote 'the text that was annotated'
     uri 'http://example.com'
-    ranges '{}'
   end
 end
