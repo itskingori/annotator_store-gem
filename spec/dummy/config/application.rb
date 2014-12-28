ee233d786dfb29b3a6abd6050487e98707b2591c
King'ori Maina <j@kingori.co>
2014-12-28 17:23:36 +0200

    We aren't using test_unit. Ever.

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -6,7 +6,6 @@ require 'action_controller/railtie'
 require 'action_mailer/railtie'
 require 'action_view/railtie'
 require 'sprockets/railtie'
-# require "rails/test_unit/railtie"
 
 Bundler.require(*Rails.groups)
 
