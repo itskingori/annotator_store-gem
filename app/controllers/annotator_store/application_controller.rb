9d28c34e0c94ec90c76c096c803cebedb5e5b568
King'ori Maina <j@kingori.co>
2015-02-06 02:38:34 +0200

    Add x-annotator-auth-token header

--- app/controllers/annotator_store/application_controller.rb
+++ app/controllers/annotator_store/application_controller.rb
@@ -8,7 +8,7 @@ module AnnotatorStore
       headers['Access-Control-Allow-Origin'] = '*'
       headers['Access-Control-Expose-Headers'] = 'ETag'
       headers['Access-Control-Allow-Methods'] = 'GET, POST, PATCH, PUT, DELETE, OPTIONS, HEAD'
-      headers['Access-Control-Allow-Headers'] = '*,x-requested-with,Content-Type,If-Modified-Since,If-None-Match'
+      headers['Access-Control-Allow-Headers'] = '*,x-requested-with,x-annotator-auth-token,Content-Type,If-Modified-Since,If-None-Match'
       headers['Access-Control-Max-Age'] = '86400'
     end
   end
